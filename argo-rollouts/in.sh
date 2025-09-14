2cf5d309b5bec9817650e3c4b072c1c8185fd919
k3sforall <denyfromall@hanmail.net>
2025-09-14 14:03:06 +0900

    rollout branch edit

--- argo-rollouts/in.sh
+++ argo-rollouts/in.sh
@@ -1,3 +1,12 @@
+kubectl create ns argo-rollouts 2>/dev/null || true
+# htpasswd 없으면: apt-get install -y apache2-utils
+USER=admin
+PASS='admin'
+kubectl -n argo-rollouts create secret generic rollouts-basic-auth \
+  --from-literal=users="$(htpasswd -nbB "$USER" "$PASS")" \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+#heml
 helm upgrade --install argo-rollouts argo/argo-rollouts \
   -n argo-rollouts --create-namespace \
   -f values-argorollouts.yaml
